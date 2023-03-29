35103ddd535f6e7b57aa471b960413c52b60baa0
Miguel Sandoval <83982572+migue-prog@users.noreply.github.com>
2023-03-28 21:22:59 -0300

    commit inicial

---
+++ public/javascript/js-front.js
@@ -0,0 +1,8 @@
+const botonEnviar = document.getElementById("boton");
+botonEnviar.addEventListener("mouseover", function(event){
+botonEnviar.style.background= "orange";
+})
+
+botonEnviar.addEventListener("mouseout", function(event){
+    botonEnviar.style.background = "lightgray";
+})
