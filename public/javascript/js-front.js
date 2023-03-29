eba82218fc498000c253bcab08908a1cef4c0053
Miguel Sandoval <83982572+migue-prog@users.noreply.github.com>
2023-03-29 14:05:13 -0300

    Delete public directory

--- public/javascript/js-front.js
+++
@@ -1,8 +0,0 @@
-const botonEnviar = document.getElementById("boton");
-botonEnviar.addEventListener("mouseover", function(event){
-botonEnviar.style.background= "blue";
-})
-
-botonEnviar.addEventListener("mouseout", function(event){
-    botonEnviar.style.background = "lightgray";
-})
